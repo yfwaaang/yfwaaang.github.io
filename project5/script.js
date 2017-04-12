f15f8a2b056d62056b32d96749780d007defb063
Yifei Wang <yifeiwang@Yifeis-MBP.home>
2017-04-12 00:00:08 -0400

    project5

---
+++ project5/script.js
@@ -0,0 +1,344 @@
+$(document).ready(function(){
+
+
+  	// $('#submit').on('click', changeBackground);
+    $('#submit').click(changeBackground);
+
+  });
+
+
+  function changeBackground(){
+
+      changeLayout();
+
+  		var letterval = $('#letter').val();
+
+  		if(letterval == 'a'){
+
+	  		$('.background1').css('background-image', 'url("pattern1.gif")');
+        $('.background2').css('background-image', 'url("pattern16.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-18.png")');
+  		}
+
+      if(letterval == 'b'){
+
+        $('.background1').css('background-image', 'url("pattern web1-02.png")');
+        $('.background2').css('background-image', 'url("pattern3.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-06.png")');
+      }
+
+      if(letterval == 'c'){
+
+        $('.background1').css('background-image', 'url("pattern3.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-18.png")');
+        $('.background3').css('background-image', 'url("pattern-23.gif")');
+      }
+
+      if(letterval == 'd'){
+
+        $('.background1').css('background-image', 'url("pattern web1-04.png")');
+        $('.background2').css('background-image', 'url("pattern web1-12.png")');
+        $('.background3').css('background-image', 'url("pattern13.gif")');
+      }
+
+      if(letterval == 'e'){
+
+        $('.background1').css('background-image', 'url("pattern5.gif")');
+        $('.background2').css('background-image', 'url("pattern-23.gif")');
+        $('.background3').css('background-image', 'url("pattern11.gif")');
+      }
+
+      if(letterval == 'f'){
+
+        $('.background1').css('background-image', 'url("pattern web1-06.png")');
+        $('.background2').css('background-image', 'url("pattern14.gif")');
+        $('.background3').css('background-image', 'url("pattern25.gif")');
+      }
+  
+      if(letterval == 'g'){
+
+        $('.background1').css('background-image', 'url("pattern-7.gif")');
+        $('.background2').css('background-image', 'url("pattern19.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-04.png")');
+      }
+
+      if(letterval == 'h'){
+
+        $('.background1').css('background-image', 'url("pattern web1-08.png")');
+        $('.background2').css('background-image', 'url("pattern10.gif")');
+        $('.background3').css('background-image', 'url("pattern22.gif")');
+      }
+
+      if(letterval == 'i'){
+
+        $('.background1').css('background-image', 'url("pattern web1-09.png")');
+        $('.background2').css('background-image', 'url("pattern25.gif")');
+        $('.background3').css('background-image', 'url("pattern22.gif")');
+      }
+
+      if(letterval == 'j'){
+
+        $('.background1').css('background-image', 'url("pattern10.gif")');
+        $('.background2').css('background-image', 'url("pattern26.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-02.png")');
+      }
+
+      if(letterval == 'k'){
+
+        $('.background1').css('background-image', 'url("pattern11.gif")');
+        $('.background2').css('background-image', 'url("pattern14.gif")');
+        $('.background3').css('background-image', 'url("pattern-23.gif")');
+      }
+
+      if(letterval == 'l'){
+
+        $('.background1').css('background-image', 'url("pattern web1-12.png")');
+        $('.background2').css('background-image', 'url("pattern web1-08.png")');
+        $('.background3').css('background-image', 'url("pattern22.gif")');
+      }
+
+      if(letterval == 'm'){
+
+        $('.background1').css('background-image', 'url("pattern13.gif")');
+        $('.background2').css('background-image', 'url("pattern3.gif")');
+        $('.background3').css('background-image', 'url("pattern20.gif")');
+      }
+
+      if(letterval == 'n'){
+
+        $('.background1').css('background-image', 'url("pattern14.gif")');
+        $('.background2').css('background-image', 'url("pattern11.gif")');
+        $('.background3').css('background-image', 'url("pattern26.gif")');
+      }
+
+      if(letterval == 'o'){
+
+        $('.background1').css('background-image', 'url("pattern15.gif")');
+        $('.background2').css('background-image', 'url("pattern20.gif")');
+        $('.background3').css('background-image', 'url("pattern21.gif")');
+      }
+
+      if(letterval == 'p'){
+
+        $('.background1').css('background-image', 'url("pattern16.gif")');
+        $('.background2').css('background-image', 'url("pattern1.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-08.png")');
+      }
+
+      if(letterval == 'q'){
+
+        $('.background1').css('background-image', 'url("pattern17.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-09.png")');
+        $('.background3').css('background-image', 'url("pattern-7.gif")');
+      }
+
+      if(letterval == 'r'){
+
+        $('.background1').css('background-image', 'url("pattern web1-18.png")');
+        $('.background2').css('background-image', 'url("pattern5.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-09.png")');
+      }
+
+      if(letterval == 's'){
+
+        $('.background1').css('background-image', 'url("pattern19.gif")');
+        $('.background2').css('background-image', 'url("pattern-7.gif")');
+        $('.background3').css('background-image', 'url("pattern17.gif")');
+      }
+
+      if(letterval == 't'){
+
+        $('.background1').css('background-image', 'url("pattern20.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-04.png")');
+        $('.background3').css('background-image', 'url("pattern web1-09.png")');
+      }
+
+      if(letterval == 'u'){
+
+        $('.background1').css('background-image', 'url("pattern1.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-02.png")');
+        $('.background3').css('background-image', 'url("pattern26.gif")');
+      }
+
+      if(letterval == 'v'){
+
+        $('.background1').css('background-image', 'url("pattern22.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-18.png")');
+        $('.background3').css('background-image', 'url("pattern-7.gif")');
+      }
+
+      if(letterval == 'w'){
+
+        $('.background1').css('background-image', 'url("pattern-23.gif")');
+        $('.background2').css('background-image', 'url("pattern5.gif")');
+        $('.background3').css('background-image', 'url("pattern15.gif")');
+      }
+
+      if(letterval == 'x'){
+
+        $('.background1').css('background-image', 'url("pattern24.gif")');
+        $('.background2').css('background-image', 'url("pattern22.gif")');
+        $('.background3').css('background-image', 'url("pattern web1-12.png")');
+      }
+
+      if(letterval == 'y'){
+
+        $('.background1').css('background-image', 'url("pattern25.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-04.png")');
+        $('.background3').css('background-image', 'url("pattern-23.gif")');
+      }
+
+      if(letterval == 'z'){
+
+        $('.background1').css('background-image', 'url("pattern26.gif")');
+        $('.background2').css('background-image', 'url("pattern web1-09.png")');
+        $('.background3').css('background-image', 'url("pattern web1-02.png")');
+      }
+  }
+
+
+function changeLayout(){
+
+      var numberval = $('#number').val();
+
+      if(numberval == '0'){
+
+        $('.background1').css("width", "70%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "30%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "70%");
+        $('.background3').css("clip-path", "polygon(70% 55%, 0% 100%, 100% 100%)");
+        $('.chair').css('background-image', 'url("poster-02.png")');
+        $('chair').css("marginLeft","55%");
+        $('chair').css("marginTop","55%");
+      }
+
+      if(numberval == '1'){
+
+        $('.background1').css("width", "40%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "60%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "40%");
+        $('.background3').css("clip-path", "polygon(40% 73%, 0% 100%, 100% 100%)");
+         $('.cabinet').css('background-image', 'url("poster-01.png")');
+        $('cabinet').css("left","55%");
+        $('cabinet').css("top","55%");
+      }
+
+      //.element {clip-path: polygon(x1 y1, x2 y2, x3 y3, ...);}
+
+
+      if(numberval == '2'){
+
+        $('.background1').css("width", "80%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "20%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "80%");
+        $('.background3').css("clip-path", "polygon(80% 58%, 0% 100%, 100% 100%)");
+         $('.pot').css('background-image', 'url("poster-03.png")');
+        $('pot').css("left","55%");
+        $('pot').css("top","55%");
+      }
+
+
+      if(numberval == '3'){
+
+        $('.background1').css("width", "10%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "90%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "10%");
+        $('.background3').css("clip-path", "polygon(10% 73%, 0% 100%, 100% 100%)");
+         $('.cabinet').css('background-image', 'url("poster-01.png")');
+        $('cabinet').css("left","55%");
+        $('cabinet').css("top","55%");
+      }
+
+
+      if(numberval == '4'){
+
+        $('.background1').css("width", "35%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "65%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "35%");
+        $('.background3').css("clip-path", "polygon(35% 77%, 0% 100%, 100% 100%)");
+         $('.chair').css('background-image', 'url("poster-02.png")');
+        $('chair').css("left","55%");
+        $('chair').css("top","55%");
+      }
+
+
+      if(numberval == '5'){
+
+        $('.background1').css("width", "65%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "35%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "65%");
+        $('.background3').css("clip-path", "polygon(65% 55%, 0% 100%, 100% 100%)");
+         $('.cabinet').css('background-image', 'url("poster-01.png")');
+        $('cabinet').css("left","55%");
+        $('cabinet').css("top","55%");
+      }
+
+
+      if(numberval == '6'){
+
+        $('.background1').css("width", "40%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "60%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "40%");
+        $('.background3').css("clip-path", "polygon(40% 55%, 0% 100%, 100% 100%)");
+         $('.pot').css('background-image', 'url("poster-03.png")');
+        $('pot').css("left","55%");
+        $('pot').css("top","55%");
+      }
+
+
+      if(numberval == '7'){
+
+        $('.background1').css("width", "50%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "50%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "50%");
+        $('.background3').css("clip-path", "polygon(50% 75%, 0% 100%, 100% 100%)");
+         $('.chair').css('background-image', 'url("poster-02.png")');
+        $('chair').css("left","55%");
+        $('chair').css("top","55%");
+      }
+
+
+      if(numberval == '8'){
+
+        $('.background1').css("width", "60%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "40%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "60%");
+        $('.background3').css("clip-path", "polygon(60% 45%, 0% 100%, 100% 100%)");
+         $('.pot').css('background-image', 'url("poster-03.png")');
+        $('pot').css("left","35%");
+        $('pot').css("top","55%");
+      }
+
+
+      if(numberval == '9'){
+
+        $('.background1').css("width", "45%"); 
+        $('.background1').css("height", "100%");
+        $('.background2').css("width", "65%");
+        $('.background2').css("height", "100%");
+        $('.background2').css("left", "45%");
+        $('.background3').css("clip-path", "polygon(45% 35%, 0% 100%, 100% 100%)");
+         $('.chair').css('background-image', 'url("poster-02.png")');
+        $('chair').css("left","30%");
+        $('chair').css("top","20%");
+      }
+    }
+  

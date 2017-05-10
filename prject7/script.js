2323242caae6e53ff4d355fd72fe84fff9a2ccd9
Yifei Wang <yifeiwang@Yifeis-MBP.wireless.newschool.edu>
2017-05-10 18:06:54 -0400

    project7

---
+++ prject7/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){ 
+
+	$('.grandpa1').mouseover(function(){
+		$('.grandpa1').addClass('mo');
+		
+	});
+
+	$('.grandpa1').mouseout(function(){
+		$('.grandpa1').removeClass('mo');
+		
+	});
+
+    $('.grandpa1').click(function(){
+        $('.grandpa').removeClass('grandpa');
+        
+    });
+
+
+   $('.grandma1').mouseover(function(){
+		$('.grandma1').addClass('mo');
+	});
+
+	$('.grandma1').mouseout(function(){
+		$('.grandma1').removeClass('mo');
+	});
+
+	  $('.grandma1').click(function(){
+        $('.grandma').removeClass('grandma');
+        
+    });
+
+
+$('.rich1').mouseover(function(){
+		$('.rich1').addClass('mo');
+	});
+
+	$('.rich1').mouseout(function(){
+		$('.rich1').removeClass('mo');
+	});
+
+	  $('.rich1').click(function(){
+        $('.rich').removeClass('rich');
+        
+    });
+
+
+$('.work1').mouseover(function(){
+		$('.work1').addClass('mo');
+	});
+
+	$('.work1').mouseout(function(){
+		$('.work1').removeClass('mo');
+	});
+
+   $('.work1').click(function(){
+        $('.work').removeClass('work');
+        
+    });
+
+
+$('.impact1').mouseover(function(){
+		$('.impact1').addClass('mo');
+	});
+
+	$('.impact1').mouseout(function(){
+		$('.impact1').removeClass('mo');
+	});
+
+  $('.impact1').click(function(){
+        $('.impact').removeClass('impact');
+        
+    });
+
+$('.fashion1').mouseover(function(){
+		$('.fashion1').addClass('mo');
+	});
+
+	$('.fashion1').mouseout(function(){
+		$('.fashion1').removeClass('mo');
+	});
+
+	  $('.fashion1').click(function(){
+        $('.fashion').removeClass('fashion');
+        
+    });
+
+	});
